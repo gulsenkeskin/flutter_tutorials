d413cbfc11637c57b5456b51bf7315d36cd91356
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-11-24 12:18:27 +0300

    added marvel_comic model

---
+++ marvel_comics/lib/domain/model/marvel_comic/marvel_comic.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:marvel_comics/domain/model/marvel_comic_format.dart';
+
+part 'marvel_comic.freezed.dart';
+part 'marvel_comic.g.dart';
+
+@freezed
+class MarvelComic with _$MarvelComic {
+  const factory MarvelComic({
+    required int id,
+    int? digitalId,
+    String? title,
+    DateTime? modified,
+    MarvelComicFormat? format,
+    @Default(<String>[]) List<String> images,
+  }) = _MarvelComic;
+
+  factory MarvelComic.fromJson(Map<String, dynamic> json) => _$MarvelComicFromJson(json);
+}
