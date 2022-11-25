62f1eb25591c14e2d8ed9f0f391f8b6272dceec7
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-11-25 12:43:36 +0300

    Update main.dart

--- marvel_comics/lib/main.dart
+++ marvel_comics/lib/main.dart
@@ -11,6 +11,7 @@ class MarvelComicsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(colorSchemeSeed: Colors.red),
       home: const MyHomePage(),
     );
