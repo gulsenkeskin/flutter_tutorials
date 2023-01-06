ae90213b862323d2f42f43bd49440a910ff5d0f2
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-06 13:01:09 +0300

    Update main.dart

--- firebase_chat/lib/main.dart
+++ firebase_chat/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_chat/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
