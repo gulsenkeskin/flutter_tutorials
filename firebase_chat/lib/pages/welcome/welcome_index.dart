7772e41dddcac8e3d47c49a43109d842c645b215
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-09 18:06:58 +0300

    Create welcome_index.dart

---
+++ firebase_chat/lib/pages/welcome/welcome_index.dart
@@ -0,0 +1,10 @@
+library welcome;
+
+export './welcome_state.dart';
+export './welcome_controller.dart';
+export './welcome_bindings.dart';
+export './welcome_view.dart';
+
+
+
+//böyle bir dosya oluşturmamızın nedeni importları temiz bir şekilde yönetmek
