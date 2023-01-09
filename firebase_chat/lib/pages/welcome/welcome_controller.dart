3df3786fa17e74feef22ab84977e459476afa53c
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-09 18:06:55 +0300

    Create welcome_controller.dart

---
+++ firebase_chat/lib/pages/welcome/welcome_controller.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_chat/pages/welcome/welcome_state.dart';
+import 'package:get/get.dart';
+
+class WelcomeController extends GetxController {
+  final state = WelcomeState();
+  WelcomeController();
+}
